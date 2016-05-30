59c05a1fb8353904b1061083ca0ec6919d92b3bb
Stephen Reid <steveyblam@hotmail.co.uk>
2016-05-30 15:59:32 +0100

    minor typos fixed

---
+++ foundations/0-debugging-and-tools/starter-code/index.js
@@ -0,0 +1,10 @@
+console.log('start finding errors');
+consoole.log('Error1 Fixed');
+aleert('Make some noise!!!!');
+var someVariable = 1;
+someVariable++;
+
+someVariable++;
+someVariable++;
+
+console.log(someVariable);
